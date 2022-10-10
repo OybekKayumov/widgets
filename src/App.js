69450d1e158b0d3ef651ae836678abc61ec74232
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-10 20:39:01 +0500

    Add Dropdown component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Accordion from './components/Accordion';
+import Dropdown from './components/Dropdown';
 import Search from './components/Search';
 
 const items = [
@@ -22,7 +23,8 @@ function App() {
     <div className="App">
     <br />
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown />
     </div>
   );
 }
