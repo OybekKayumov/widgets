b9e5388d31aeb6dc9083bbb89e7c1751f25942bf
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-10 20:25:57 +0500

    Add debounced term useEffects

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,38 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState('programming'); // vN157
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000)
+    
+    return () => {
+      clearTimeout(timerId);
+    }
+  }, [term])
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: debouncedTerm
+        },
+      });
+
+      // setResults();
+      setResults(data.query.search);
+    };
+
+    search();
+
+  }, [debouncedTerm])
   
   // const [term, setTerm] = useState('');
   // const [termTwo, setTermTwo] = useState('');
@@ -51,37 +82,41 @@ const Search = () => {
 
   console.log('results: ', results);
 
-  useEffect(() => {
-    const search = async () => {
-      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
-        params: {
-          action: 'query',
-          list: 'search',
-          origin: '*',
-          format: 'json',
-          srsearch: term
-        },
-      });
+  // useEffect(() => {
+    // const search = async () => {
+    //   const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+    //     params: {
+    //       action: 'query',
+    //       list: 'search',
+    //       origin: '*',
+    //       format: 'json',
+    //       srsearch: term
+    //     },
+    //   });
 
-      // setResults();
-      setResults(data.query.search);
-    };
+    //   // setResults();
+    //   setResults(data.query.search);
+    // };
 
-    // if term is an empty string, will NOT search,
-    // if TERM has some chars inside, we will do a search 
-    const timeoutId = setTimeout(() => {
-      if (term) {
-        search();
-      }
-    }, 500)
-    
-    // search();
+  //   if (term && !results.length) {
+  //     search();
+  //   } else {
+  //     // if term is an empty string, will NOT search,
+  //     // if TERM has some chars inside, we will do a search 
+  //     const timeoutId = setTimeout(() => {
+  //       if (term) {
+  //         search();
+  //       }
+  //     }, 500)
+      
+  //     // search();
 
-    return () => {
-      clearTimeout(timeoutId)
-    }
+  //     return () => {
+  //       clearTimeout(timeoutId)
+  //     }
+  //   }    
 
-  }, [term])
+  // }, [term, results.length])
 
   const renderedResults = results.map((result) => {
     return (
