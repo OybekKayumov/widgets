4a24116baf9cb8094cdc193c343d69656f302c3b
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-11 07:46:45 +0500

    Add input in translate

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -18,8 +18,12 @@ const options = [
 
 const Translate = () => {
   const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState('');
+
   return (
     <div>
+      <input value={text} onChange={(e) => setText(e.target.value)}/>
+
       <Dropdown
         label='Select a Language' 
         selected={language}
