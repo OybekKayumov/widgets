2f401c5b6053411dfdb01bde523b246ca3c31dd4
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-11 20:16:05 +0500

    Add changing url

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
+    // no update page
     event.preventDefault();
+
+    // change url without updating
+    window.history.pushState({}, '', href)
   }
 
   return (
